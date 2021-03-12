9c168c8bc8c466aa526129c088e69a20377c3785
N3cr0 <19351579+Necronhym@users.noreply.github.com>
2021-03-13 00:52:10 +0100

    Inital commit

---
+++ necromancpp.h
@@ -0,0 +1,17 @@
+#ifndef NECROMANCY_H
+#define NECROMANCY_H
+typedef bool blood;
+typedef int ichor;
+typedef float flesh;
+typedef char chalk;
+#define servant struct
+#define creature class
+#define eternal const
+#define evil true
+#define good false
+#define spirit &
+#define body *
+namespace necronomicon = std;
+#define moan cout
+#define groan endl
+#endif
